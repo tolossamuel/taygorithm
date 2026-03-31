69f32709b9988568b9b6560bb05400c739a1e2e1
tolossamuel <robsen.teshoma@astu.edu.et>
2026-03-31 14:00:00 +0300

    chore: containerize application with multi-stage Docker setup

--- src/ComponentUpdates.jsx
+++ src/ComponentUpdates.jsx
@@ -6,3 +6,5 @@ export const ComponentDay1 = () => <div>Update Day 1</div>;
 export const ComponentDay2 = () => <div>Update Day 2</div>;
 
 export const ComponentDay3 = () => <div>Update Day 3</div>;
+
+export const ComponentDay4 = () => <div>Update Day 4</div>;
