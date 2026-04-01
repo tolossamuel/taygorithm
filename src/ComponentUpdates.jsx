634c4c17660ee0d7671ab88e4baca345665b2b2f
tolossamuel <robsen.teshoma@astu.edu.et>
2026-04-01 14:00:00 +0300

    feat: integrate advanced role-based access control (RBAC)

--- src/ComponentUpdates.jsx
+++ src/ComponentUpdates.jsx
@@ -8,3 +8,5 @@ export const ComponentDay2 = () => <div>Update Day 2</div>;
 export const ComponentDay3 = () => <div>Update Day 3</div>;
 
 export const ComponentDay4 = () => <div>Update Day 4</div>;
+
+export const ComponentDay5 = () => <div>Update Day 5</div>;
